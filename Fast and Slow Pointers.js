84fbf1794ce1be0dbc3e47428e21d5856cc1443c
Amrit Raj <amritrazzz@gmail.com>
2021-02-24 11:35:46 +0530

    added Fast and Slow Pointers

---
+++ Fast and Slow Pointers.js
@@ -0,0 +1,6 @@
+/*
+The fast and slow pointer approach, also known as the hare and tortoise algorithm, is a pointer
+algorithm that uses two pointers which move through the array (or sequence/LinkedList) at different speeds.
+This approach is quite useful when dealing with cyclic LinkedLists or arrays
+
+*/
