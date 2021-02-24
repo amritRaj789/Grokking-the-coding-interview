ca5090a9cfa27be9afdfdc97989de4575b393e15
Amrit Raj <amritrazzz@gmail.com>
2021-02-24 13:31:10 +0530

    added problem 4

--- 2 pointers.js
+++ 2 pointers.js
@@ -48,3 +48,28 @@ const make_squares = function(arr) {
   }
   return result;
 };
+
+//Triplet Sum Close to Target
+const triplet_sum_close_to_target = function(arr, target_sum) {
+  arr.sort((a,b) => a-b);
+  let closestSum = +Infinity;
+  for(let i = 0; i < arr.length-2; i++){
+  	let left = i+1;
+  	let right = arr.length-1;
+  	while(left < right){
+  		sum = arr[i] + arr[left] + arr[right];
+  		if( sum < target_sum)
+  			left++;
+  		else if(sum > target_sum)
+  			right--;
+  		else
+  			return sum;
+  		if(Math.abs(closestSum - target_sum) > Math.abs(sum - target_sum))
+  			closestSum = sum;
+  		else if(Math.abs(closestSum - target_sum) === Math.abs(sum - target_sum)){
+  			closestSum = Math.min(sum, closestSum);
+  		}
+  	}
+  }
+  return closestSum;
+};
