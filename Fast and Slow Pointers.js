3fd457815738553f1cac828867ce9b7f28d24190
Amrit Raj <amritrazzz@gmail.com>
2021-03-01 12:41:07 +0530

    Problem 1

--- Fast and Slow Pointers.js
+++ Fast and Slow Pointers.js
@@ -4,3 +4,16 @@ algorithm that uses two pointers which move through the array (or sequence/Linke
 This approach is quite useful when dealing with cyclic LinkedLists or arrays
 
 */
+
+// LinkedList Cycle (easy)
+const has_cycle = function(head) {
+	let fast = head;
+	let slow = head;
+	while(fast.next !== null && fast.next.next !== null){
+		slow = slow.next;
+		fast = fast.next.next;
+		if(fast === slow)
+			return true;
+	}
+	return false;
+}
