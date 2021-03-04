bc0e87bf13ad98db507043dbf02410648761c693
amritRaj789 <amritrazzz@gmail.com>
2021-03-04 19:40:05 +0530

    Added alternate solution

--- Fast and Slow Pointers.js
+++ Fast and Slow Pointers.js
@@ -114,3 +114,51 @@ const is_palindromic_linked_list= function(head) {
   return true;
 
 };
+//but the above method uses O(N) space
+
+//The below method uses constant space
+function is_palindromic_linked_list(head) {
+  if (head === null || head.next === null) {
+    return true;
+  }
+
+  // find middle of the LinkedList
+  let slow = head,
+    fast = head;
+  while ((fast !== null && fast.next !== null)) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  headSecondHalf = reverse(slow); // reverse the second half
+  // store the head of reversed part to revert back later
+  copyHeadSecondHalf = headSecondHalf;
+
+  // compare the first and the second half
+  while ((head !== null && headSecondHalf !== null)) {
+    if (head.value !== headSecondHalf.value) {
+      break; // not a palindrome
+    }
+
+    head = head.next;
+    headSecondHalf = headSecondHalf.next;
+  }
+  reverse(copyHeadSecondHalf); // revert the reverse of the second half
+
+  if (head === null || headSecondHalf === null) { // if both halves match
+    return true;
+  }
+
+  return false;
+}
+
+function reverse(head) {
+  let prev = null;
+  while (head !== null) {
+    next = head.next;
+    head.next = prev;
+    prev = head;
+    head = next;
+  }
+  return prev;
+}
