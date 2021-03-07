7af34f6be30552079ecd1247a1726c62acf66a11
amritRaj789 <amritrazzz@gmail.com>
2021-03-07 10:02:44 +0530

    Problem 1

--- Merge Intervals.js
+++ Merge Intervals.js
@@ -11,3 +11,48 @@
 
 //Problem 1
 //Given a list of intervals, merge all the overlapping intervals to produce a list that has only mutually exclusive intervals.
+
+
+/*class Interval {
+  constructor(start, end) {
+    this.start = start;
+    this.end = end;
+  }
+  print_interval() {
+    process.stdout.write(`[${this.start}, ${this.end}]`);
+  }
+}
+*/
+function merge(intervals) {
+  if (intervals.length < 2) {
+    return intervals;
+  }
+  // sort the intervals on the start time
+  intervals.sort((a, b) => a.start - b.start);
+
+  const mergedIntervals = [];
+  let start = intervals[0].start,
+    end = intervals[0].end;
+  for (i = 1; i < intervals.length; i++) {
+    const interval = intervals[i];
+    if (interval.start <= end) { // overlapping intervals, adjust the 'end'
+      end = Math.max(interval.end, end);
+    } else { // non-overlapping interval, add the previous interval and reset
+      mergedIntervals.push(new Interval(start, end));
+      start = interval.start;
+      end = interval.end;
+    }
+  }
+  // add the last interval
+  mergedIntervals.push(new Interval(start, end));
+  return mergedIntervals;
+}
+
+/*merged_intervals = merge([new Interval(1, 4), new Interval(2, 5), new Interval(7, 9)]);
+result = "";
+for(i=0; i < merged_intervals.length; i++) {
+  result += merged_intervals[i].get_interval() + " ";
+}
+console.log(`Merged intervals: ${result}`)*/
+
+
