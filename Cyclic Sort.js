03a14f1590a3e0a13bca43cfc876764688f6c8a9
amritRaj789 <amritrazzz@gmail.com>
2021-03-09 13:00:54 +0530

    Problem 1

--- Cyclic Sort.js
+++ Cyclic Sort.js
@@ -24,3 +24,32 @@ const cyclic_sort = function (nums){
 	}
 }
 
+/* Problem 1: Find the missing number
+We are given an array containing ‘n’ distinct numbers taken from the range 0 to ‘n’. 
+Since the array has only ‘n’ numbers out of the total ‘n+1’ numbers, find the missing number.
+*/
+//Mathematics way of doing it. This one works since there is only one missing number
+const find_missing_number = function(nums) {
+  const sum = nums.reduce((total, num) => total + num);
+  const total = (nums.length*(nums.length+1))/2;
+  return total - sum;
+};
+
+//Cyclic sort way of doing it.
+const find_missing_number = function (nums){
+	let i = 0;
+	let n = nums.length;
+	while(i < n){
+		while(nums[i] < n && nums[i] !== i){
+			const temp = nums[nums[i]];
+			nums[nums[i]] = nums[i];
+			nums[i] = temp;
+		}
+		i++;
+	}
+	for(let j = 0; j < n; j++){
+		if(nums[j] !== j)
+			return j;
+	}
+	return n;
+}
