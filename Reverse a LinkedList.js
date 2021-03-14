e71549201f8ece881a9eaeb133e952a1218e3351
amritRaj789 <amritrazzz@gmail.com>
2021-03-14 18:19:56 +0530

    Problem 2

--- Reverse a LinkedList.js
+++ Reverse a LinkedList.js
@@ -20,3 +20,34 @@ class Node {
  	}
  	return prev;
  }
+
+
+ //Reverse A sub-list
+
+
+const reverse_sub_list = function(head, p, q) {
+	let node = head;
+	while(p > 2){
+		node = node.next;
+		p--;
+		q--;
+	}
+	let copyNode = node;
+	let prev = null;
+	let current = node.next;
+	let copyHead = current;
+	q--;
+	while(q > 0){
+		const temp = current.next;
+		current.next = prev;
+		prev = current;
+		current = temp;
+		q--;
+	}
+	copyNode.next = prev;
+	copyHead.next = current;
+	return head;
+};
+// won't work if p = 1
+
+// Reverse every
