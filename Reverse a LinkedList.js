31383f272e6f374bf34909a093245fd9b91f6b8a
amritRaj789 <amritrazzz@gmail.com>
2021-03-16 14:37:41 +0530

    added all problems and challenges in Reverse-LinkedList

--- Reverse a LinkedList.js
+++ Reverse a LinkedList.js
@@ -80,3 +80,74 @@ If, in the end, you are left with a sub-list with less than ‘k’ elements, re
   }
   return head;
 }
+
+// Problem Challenge 1
+
+/*Reverse alternating K-element Sub-list
+
+Given the head of a LinkedList and a number ‘k’, reverse every alternating ‘k’ sized sub-list starting from the head.
+If, in the end, you are left with a sub-list with less than ‘k’ elements, reverse it too.
+*/
+
+const reverse_alternate_k_elements = function(head, k) {
+  
+	let current = head;
+	let copyLink;
+	let count = 0;
+	let copyLink2;
+	let i;
+	while(current !== null){
+		i = k;
+		let prev = null;
+		copyLink = current;
+		while(i > 0 && current !== null){
+			let temp = current.next;
+			current.next = prev;
+			prev = current;
+			current = temp;
+			i--;
+		}
+		count++;
+		if(count === 1)
+			head = prev;
+		copyLink.next = current;
+		if(count >= 2){
+			copyLink2.next = prev;
+		}
+		i = k;
+		while(i > 1 && current !== null){
+			current = current.next;
+			i--;
+		}
+		copyLink2 = current;
+		current = (current === null) ? current : current.next;
+	}
+	return head;
+};
+
+/*Problem Challenge 2
+
+Rotate a Linked List
+Given the head of a singly linked list and a number 'K', rotate the LinkedList to the right by 'K' nodes
+*/const rotate = function(head, rotations) {
+  // reach the end
+  let fast = head;
+  let k = rotations;
+  while(k > 0){
+  	fast = fast.next;
+  	k--;
+  }
+  let slow = head;
+  while(fast.next !== null){
+  	fast = fast.next;
+  	slow = slow.next;
+  }
+  const temp = slow.next;
+  fast.next = head;
+  slow.next = null;
+  return temp;
+};
+
+
+
+
