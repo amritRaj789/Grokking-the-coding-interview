bb8f277a8ebe461bc705d5a096d6e0c771787462
amritRaj789 <amritrazzz@gmail.com>
2021-04-19 18:02:57 +0530

    --added summary of patterns

---
+++ Two Heaps.js
@@ -0,0 +1,18 @@
+Prerequisite: 
+We are given a set of elements such that we can divide them into two parts.
+To solve the problem, we are interested in knowing the smallest element in one
+part and the biggest element in the other part.
+
+Problem 1
+
+class MedianOfAStream {
+  insert_num(num) {
+   // TODO: Write your code here
+   return -1;
+  }
+
+  find_median(self) {
+    // TODO: Write your code here
+    return 0.0
+  }
+};
