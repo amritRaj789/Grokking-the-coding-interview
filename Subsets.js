ca93235ee50ab8d50d1b880c0fb64424238e64b7
amritRaj789 <amritrazzz@gmail.com>
2021-04-24 22:48:08 +0530

    --added Subsets pattern

---
+++ Subsets.js
@@ -0,0 +1,9 @@
+// A huge number of coding interview problems involve dealing with 
+//Permutations, Combinations of a given set of elements. This pattern describes an efficient BFS approach
+
+//
+const find_subsets = function(nums) {
+  subsets = [];
+  // TODO: Write your code here
+  return subsets;
+};
