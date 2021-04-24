3eeb100c3714867faaf3f69a2405b0d8323cc11f
amritRaj789 <amritrazzz@gmail.com>
2021-04-25 00:09:14 +0530

    --Problem 1. Subsets

--- Subsets.js
+++ Subsets.js
@@ -1,9 +1,49 @@
 // A huge number of coding interview problems involve dealing with 
 //Permutations, Combinations of a given set of elements. This pattern describes an efficient BFS approach
 
-//
+// Problem 1
+// Given a set with distinct elements, find all of its distinct subsets
+
+// my way: dfs
 const find_subsets = function(nums) {
-  subsets = [];
-  // TODO: Write your code here
-  return subsets;
+	let result = [];
+	function dfs(i, arr){
+		if(i === nums.length){
+			result.push([...arr]);
+			return
+		}
+		dfs(i+1, [...arr, nums[i]]);
+		dfs(i+1, [...arr]);
+	}
+	dfs(0, []);
+	return result;
 };
+
+// bfs recursive
+const find_subsets = function (nums){
+	let result = [[]];
+	function bfs(index){
+		if(index === nums.length)
+			return
+		let length = result.length;
+		for(let i = 0; i < length; i++){
+			result.push([...result[i], nums[index]]);
+		}
+		bfs(index+1);
+	}
+	bfs(0);
+	return result;
+}
+
+// bfs iterative
+const find_subsets = function (nums){
+	let result = [[]];
+	for(let i = 0; i < nums.length; i++){
+		let length = result.length;
+		for(let j = 0; j < length; j++){
+			result.push([...result[j], nums[i]]);
+		}
+	}
+	return result;
+}
+// O(N*2^N) space and time
