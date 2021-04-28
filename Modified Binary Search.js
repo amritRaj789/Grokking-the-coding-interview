9d626b44abe28f556b5dd7af41f7c473c9bba3dd
amritRaj789 <amritrazzz@gmail.com>
2021-04-28 11:07:27 +0530

    --P2 Ceililng of a Number

--- Modified Binary Search.js
+++ Modified Binary Search.js
@@ -34,3 +34,29 @@ const binary_search = function(arr, key) {
 		return -1
 	}	// ascending
 };
+
+
+// problem 2
+// Ceiling of a Number
+//Given an array of numbers sorted in an ascending order, find the ceiling of a given number ‘key’. 
+//The ceiling of the ‘key’ will be the smallest element in the given array greater than or equal to the ‘key’.
+
+
+const search_ceiling_of_a_number = function(arr, key) {
+	if(arr[0] >= key)
+		return 0
+	else if(arr[arr.length-1] < key)
+		return -1;
+	let left = 0;
+	let right = arr.length-1;
+	while(left <= right){
+		mid = Math.floor((left+right)/2);
+		if(arr[mid] === key)
+			return mid
+		else if(key < arr[mid])
+			right = mid-1;
+		else
+			left = mid+1;
+	}
+	return left;
+};
